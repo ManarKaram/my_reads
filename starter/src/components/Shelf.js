60c8c2592234349f22deb9125e5f9da74eae79ed
Manar AboElKhair <manar.aboelkhair@vodafone.com>
2023-03-13 11:51:06 +0200

    new structure and try new environment

--- starter/src/components/Shelf.js
+++ starter/src/components/Shelf.js
@@ -1,20 +1,8 @@
 import React, { Component } from "react";
 import Book from "./Book";
 
-class Shelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-           <Book/>
-           
-          </ol>
-        </div>
-      </div>
-    );
-  }
+const Shelf = () => {
+  
 }
 
 export default Shelf;
