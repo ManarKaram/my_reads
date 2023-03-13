7495faae80b5a8dfca16ece311a94411c50af101
Manar AboElKhair <manar.aboelkhair@vodafone.com>
2023-03-13 12:25:18 +0200

    added book, shelf and shelves

--- starter/src/components/Shelves.js
+++ starter/src/components/Shelves.js
@@ -1,7 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Shelf from "./Shelf";
 
 const Shelves = () => {
-    
-}
+  // state of shelves
+  const currentlyReading = [];
+  const whatToRead = [];
+  const read = [];
+
+  return (
+    <>
+      <Shelf />
+      <Shelf />
+    </>
+  );
+};
 
 export default Shelves;
